f09008bc991e87cbb26a0f75a50ba97669a5b7d8
NuanKi <emvoh@proton.me>
2026-02-08 01:12:01 -0600

    - Improved Gui - Updated Textures - Improved Mixin

--- src/main/java/me/emvoh/midviewcell/mixin/MixinSlotRestrictedInput_ViewCell.java
+++ src/main/java/me/emvoh/midviewcell/mixin/MixinSlotRestrictedInput_ViewCell.java
@@ -1,34 +1,34 @@
 package me.emvoh.midviewcell.mixin;
 
-import appeng.api.definitions.IItemDefinition;
 import appeng.container.slot.SlotRestrictedInput;
+import appeng.items.storage.ItemViewCell;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = SlotRestrictedInput.class, remap = false)
 public abstract class MixinSlotRestrictedInput_ViewCell {
 
-    @Redirect(
-            method = {
-                    "isItemValid(Lnet/minecraft/item/ItemStack;)Z",
-                    "func_75214_a(Lnet/minecraft/item/ItemStack;)Z"
-            },
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lappeng/api/definitions/IItemDefinition;isSameAs(Lnet/minecraft/item/ItemStack;)Z",
-                    remap = false
-            ),
+    @Inject(
+            method = { "isItemValid(Lnet/minecraft/item/ItemStack;)Z",
+                       "func_75214_a(Lnet/minecraft/item/ItemStack;)Z" },
+            at = @At("RETURN"),
+            cancellable = true,
             remap = false
     )
-    private boolean midviewcell$viewCellAcceptMyItem(IItemDefinition def, ItemStack stack) {
-        if (def.isSameAs(stack)) {
-            return true;
+    private void midviewcell$acceptAnyItemViewCell(final ItemStack stack, final CallbackInfoReturnable<Boolean> cir) {
+        if (cir.getReturnValue()) {
+            return;
         }
 
-        SlotRestrictedInput self = (SlotRestrictedInput) (Object) this;
-        return self.getPlaceableItemType() == SlotRestrictedInput.PlacableItemType.VIEW_CELL
-                && stack.getItem() instanceof me.emvoh.midviewcell.items.ModItemViewCell;
+        final SlotRestrictedInput self = (SlotRestrictedInput) (Object) this;
+
+        if (self.getPlaceableItemType() == SlotRestrictedInput.PlacableItemType.VIEW_CELL
+                && !stack.isEmpty()
+                && stack.getItem() instanceof ItemViewCell) {
+            cir.setReturnValue(true);
+        }
     }
 }
